72d4b8b5d60a6c093d357c6604e0c2db48e6e01f
unknown <achahourmohamed15@gmail.com>
2025-03-14 14:24:41 +0000

    Orderscardfk

--- src/pages/orders_pages/orders.jsx
+++ src/pages/orders_pages/orders.jsx
@@ -1,5 +1,39 @@
-export default function Orders(){
-    return(
-        <div>Orders</div>
-    )
+// export default function Orders(){
+//     return(
+//         <div>Orders</div>
+//     )
+// }
+
+import { useEffect, useState } from "react";
+import ordersCard from "./ordersCard";
+// import '../../css_files/orders.css' ;
+
+
+export default function Orders() {
+    const [orders, setOrders] = useState([])
+
+    useEffect(() => {
+        const get_Orders = async () => {
+            try {
+                const response = await fetch('http://127.0.0.1:8000/api/orders');
+                const data = await response.json();
+                console.log(data)
+                setOrders(data.data);
+            }
+            catch (error) {
+                console.error('error fetching users : ', error)
+            }
+        };
+        get_Orders();
+    }, []);
+
+    return (
+
+        <div className="orders">
+            {orders.map((orders, index) => (
+                <ordersCard key={index} orders={orders} />
+            ))}
+        </div>
+
+    );
 }
