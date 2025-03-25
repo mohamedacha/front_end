0c5e76846cdd6b802ba30b0e490c5929cd4007b2
ouichouani <ouichouaniabdlhakim@gmal.com>
2025-03-25 00:53:37 +0000

    update home page style

--- src/parties/home.jsx
+++ src/parties/home.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import "../css_files/Home.css";
+import { Link } from "react-router-dom";
 
 const SearchBar = () => {
   return (
-    <div className="search-bar">
-      <input type="text" placeholder="Search" className="search-input" />
-      <button className="search-button">🔍</button>
+    <div className="search-bar-home">
+      <input type="text" placeholder="Search" className="search-input-home" />
+      <button className="search-button-home">🔍</button>
     </div>
   );
 };
@@ -21,12 +22,15 @@ const Section = ({ image, title }) => {
 
 const Home = () => {
   return (
-    <div className="Home">
+    <div className="Home_page">
       <SearchBar />
-      <div className="sections-container">
-        <Section image="/services.png" title="discover our services" />
-        <Section image="/products.png" title="discover our products" />
-      </div>
+      <img src="img/home2.jpeg" alt="" />
+
+      <p className="home_page_title" >DISCOVER OUR 
+         <Link to ='/products' > PRODUCTS </Link>
+        AND
+        <Link to ='/services' > SERVICES </Link>
+      </p>
     </div>
   );
 };
